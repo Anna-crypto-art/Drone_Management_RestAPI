c696482e709727b9f142bbeaa740ba1ade1f8d41
jgauchel <jakry01@gmail.com>
2021-05-07 00:50:57 +0200

    draw first dt, success

--- src/app/plant/csp-ptc/plant-view-csp-ptc.vue
+++ src/app/plant/csp-ptc/plant-view-csp-ptc.vue
@@ -1,5 +1,5 @@
 <template>
-  <open-layers v-if="hasLayers" :layers="layers" :zoom="15"></open-layers>
+  <open-layers v-if="hasLayers" :layers="layers" :zoom="5" :center="center"></open-layers>
 </template>
 
 <script lang="ts">
@@ -8,6 +8,7 @@ import { Component, Prop } from 'vue-property-decorator';
 import { LayerType, OpenLayers } from 'volateq-geovisualization';
 import volateqApi from '@/app/shared/services/volateq-api/volateq-api';
 import { PlantSchema } from '@/app/shared/services/volateq-api/api-schemas/plant-schema';
+import { Style, Stroke } from 'ol/style';
 
 
 @Component({
@@ -19,41 +20,28 @@ export default class AppPlantViewCspPtc extends Vue {
   @Prop() plant!: PlantSchema;
 
   layers: LayerType[] = [];
+  center = [0, 0];
 
   async created() {
     this.layers.push({
       name: "dt",
       type: "geojson",
+      style: () => {
+        return new Style({
+          stroke: new Stroke({
+            color: 'rgba(0, 0, 0, 1)',
+            width: 1
+          })
+        });
+      },
+      geoJSONOptions: {
+        dataProjection: 'EPSG:4326',
+        featureProjection: 'EPSG:3857'
+      },
       geoJSONLoader: (): Promise<Record<string, unknown>> => {
         console.log('load dt...');
 
         return volateqApi.getFieldgeometry(this.plant.customer_id, this.plant.id)
-
-        // return {
-        //   type: "FeatureCollection",
-        //   crs: {
-        //     type: 'name',
-        //     properties: {
-        //       'name': 'EPSG:3857',
-        //     },
-        //   },
-        //   features: [
-        //     {
-        //       type: "Feature",
-        //       geometry: {
-        //         type: "Polygon",
-        //         coordinates: [
-        //           [
-        //             [0, 118],
-        //             [2920, 118],
-        //             [2920, 0],
-        //             [0, 0]
-        //           ]
-        //         ]
-        //       }
-        //     }
-        //   ]
-        // };
       }
     });
   }
