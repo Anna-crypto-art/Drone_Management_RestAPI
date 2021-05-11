232e47ee414f7e603c22548949fb5f03ef9162de
jgauchel <jakry01@gmail.com>
2021-05-11 11:37:36 +0200

    add components as layer

--- src/app/plant/csp-ptc/plant-view-csp-ptc.vue
+++ src/app/plant/csp-ptc/plant-view-csp-ptc.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <b-form-checkbox :checked="showPcs" :change="toggleShowPCS">{{ $t('show-pcs') }}</b-form-checkbox>
-    <open-layers v-if="hasLayers" :layers="layers" :zoom="5" :center="[0, 0]"></open-layers>
+    <open-layers v-if="hasLayers" :layers="layers"></open-layers>
   </div>
 </template>
 
@@ -25,47 +24,93 @@ export default class AppPlantViewCspPtc extends Vue {
   @Prop() plant!: PlantSchema;
 
   layers: LayerType[] = [];
-  showPCS = true;
+  showPCS = false;
 
   async created() {
-    this.layers.push({
-      name: "SCAs",
-      type: "geojson",
-      style: (feature: FeatureLike, resolution: number) => {
-        console.log(this.showPCS)
+    const geoJSONOptions = { dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857' };
+    const showText = (feature: FeatureLike): Text | undefined => {
+      return new Text({
+        text: this.showPCS && feature.get('name') || '',
+        overflow: true,
+        rotation: -(Math.PI / 2),
+      }) || undefined
+    }
 
-        return new Style({
-          stroke: new Stroke({
-            color: 'rgba(0, 0, 0, 1)',
-            width: 1,
-          }),
-          text: new Text({
-            text: this.showPCS && feature.get('name') || '',
-            overflow: true,
-            rotation: -(Math.PI / 2),
-          }) || undefined
-        });
+    this.layers.push(
+      {
+        name: this.$t('world-map').toString(),
+        type: "osm",
+        selected: true,
+        childLayers: undefined
       },
-      geoJSONOptions: {
-        dataProjection: 'EPSG:4326',
-        featureProjection: 'EPSG:3857'
+      {
+        name: this.$t('pcs').toString(),
+        type: "custom",
+        customLoader: () => undefined,
+        onSelected: (selected: boolean) => { this.showPCS = selected; },
+        selected: false
       },
-      geoJSONLoader: (): Promise<Record<string, unknown>> => {
-        console.log('load dt...');
+      {
+        name: this.$t('components').toString(),
+        type: "group",
+        childLayers: [
+          {
+            name: this.$t('solar-collector-assembly').toString(),
+            type: "geojson",
+            style: (feature: FeatureLike, resolution: number) => {
+              return new Style({
+                stroke: new Stroke({
+                  color: '#888888',
+                  width: 1,
+                }),
+                text: showText(feature)
+              });
+            },
+            geoJSONOptions,
+            geoJSONLoader: () => volateqApi.getComponentsGeoVisual(this.plant.id, [AnalysisResultComponent.CSP_PTC_SCA]),
+            selected: true,
+            autoZoom: true,
+          },
+          {
+            name: this.$t("absorber-tubes").toString(),
+            type: "geojson",
+            style: (feature: FeatureLike) => {
+              return new Style({
+                stroke: new Stroke({
+                  color: '#000000',
+                  width: 1,
+                }),
+                text: showText(feature),
+              });
+            },
+            geoJSONOptions,
+            geoJSONLoader: () => volateqApi.getComponentsGeoVisual(this.plant.id, [AnalysisResultComponent.CSP_PTC_ABSORBER]),
+            selected: false
+          },
+          {
+            name: this.$t("single-collector-elements").toString(),
+            type: "geojson",
+            style: (feature: FeatureLike) => {
+              return new Style({
+                fill: new Fill({
+                  color: '#aaaaaa',
+                }),
+                text: showText(feature),
+              });
+            },
+            geoJSONOptions,
+            geoJSONLoader: () => volateqApi.getComponentsGeoVisual(this.plant.id, [AnalysisResultComponent.CSP_PTC_SCE]),
+            selected: false,
+          },
+        ]
+      }
+    );
 
-        return volateqApi.getComponentsGeoVisual(this.plant.id, [AnalysisResultComponent.CSP_PTC_SCA]);
-      },
-      selected: true,
-      autoZoom: true
-    });
+    console.log(this.layers);
   }
 
   get hasLayers(): boolean {
     return this.layers.length > 0;
-  }
-
-  toggleShowPCS(showPcs: boolean) {
-    this.showPCS = showPcs;
   }
 }
 </script>
