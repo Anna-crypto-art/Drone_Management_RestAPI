32e9f10edfd48df1f640ae6598dbd8c390df89c4
Jeremy Gauchel <jeremy@gauchels.de>
2023-01-02 13:24:26 +0100

    fe

---
+++ src/app/settings/user-profile/user-profile.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app-user-profile">
+    <app-box :title="$t('change-authentication-method')">
+      
+    </app-box>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component } from "vue-property-decorator";
+
+import { BaseAuthComponent } from "@/app/shared/components/base-auth-component/base-auth-component";
+import AppBox from "@/app/shared/components/app-box/app-box.vue"
+import AppButton from "@/app/shared/components/app-button/app-button.vue";
+
+@Component({
+  name: "app-user-profile",
+  components: {
+    AppBox,
+    AppButton,
+  },
+})
+export default class AppUserProfile extends BaseAuthComponent {
+
+}
+</script>
+
+<style></style>
