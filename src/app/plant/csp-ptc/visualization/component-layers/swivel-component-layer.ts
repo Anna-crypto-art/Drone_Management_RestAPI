e802e468e6580f30839122827b5e4aff222d6132
Jeremy Gauchel <jeremy@gauchels.de>
2023-01-27 18:03:59 +0100

    circle for swivels

--- src/app/plant/csp-ptc/visualization/component-layers/swivel-component-layer.ts
+++ src/app/plant/csp-ptc/visualization/component-layers/swivel-component-layer.ts
@@ -1,6 +1,6 @@
 import { ApiComponent } from "@/app/shared/services/volateq-api/api-components/api-components";
 import { ComponentLayer } from "../../../shared/visualization/layers/component-layer";
-import { Style, Stroke } from "ol/style";
+import { Style, Stroke, Circle, Fill } from "ol/style";
 import { FeatureLike } from "ol/Feature";
 import { KeyFigureColors } from "@/app/plant/shared/visualization/layers/types";
 
@@ -13,9 +13,13 @@ export class SwivelComponentLayer extends ComponentLayer {
 
   public getStyle(feature: FeatureLike): Style {
     return new Style({
-      stroke: new Stroke({
-        color: KeyFigureColors.black,
-        width: 2,
+      image: new Circle({
+        radius: 3,
+        fill: new Fill({ color: '#888888' }),
+        stroke: new Stroke({
+          color: KeyFigureColors.black,
+          width: 1,
+        }),
       }),
       text: this.showText(feature),
     });
