2f0dac96eb7afbadd24de3aa6b1ef3ab85728c6a
Lenium37 <hirth.johannes@gmx.de>
2023-07-20 18:19:58 +0200

    v6.7.0

--- src/app/shared/services/volateq-api/api-tasks.ts
+++ src/app/shared/services/volateq-api/api-tasks.ts
@@ -1,5 +1,5 @@
 export enum ApiTasks {
   import_analysis_results = "import_analysis_results",
-  validate_plant_metadata = "validate_plant_metadata",
   run_qfly_wizard = "run_qfly_wizard",
+  download_analysis_files = "download_analysis_files",
 }
